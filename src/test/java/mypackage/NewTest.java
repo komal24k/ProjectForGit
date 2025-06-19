157ccf8d18054b00a2cc3b39515e59fcf219c2ab
komal24k <komalkasar82@gmail.com>
2025-06-19 17:07:09 +0530

    Added

--- src/test/java/mypackage/NewTest.java
+++ src/test/java/mypackage/NewTest.java
@@ -1,10 +1,19 @@
 package mypackage;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class NewTest {
   @Test
   public void f() {
 	  System.out.println("Hi Hello");
+  }
+  
+  @Test
+  public void LoginTest() {
+
+	  WebDriver driver=new ChromeDriver();
+	  driver.get("https://ciitstudent.com");
   }
 }
