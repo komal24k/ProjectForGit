0a2172008671dcab7fc7feb37be1d5a7eb93a12e
komal24k <komalkasar82@gmail.com>
2025-06-21 08:55:44 +0530

    sdf

--- src/test/java/mypackage/NewTest.java
+++ src/test/java/mypackage/NewTest.java
@@ -5,38 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class NewTest {
-  @Test
+
+	@Test
   public void f() {
 	  System.out.println("Hello");
   }
   
-  @Test
-  public void LoginTest() {
-
-	  WebDriver driver=new ChromeDriver();
-	  driver.get("https://ciitstudent.com");
- }
-  @Test
-  public void LoginTest2() {
-
-	  WebDriver driver=new ChromeDriver();
-  }  
-	@Test
-  public void LoginTest3() {
-
-	  WebDriver driver=new ChromeDriver();
-}
 
 
-
-	@Test
-  public void LoginTest4() {
-
-	  WebDriver driver=new ChromeDriver();
-}
-	@Test
-  public void LoginTest5() {
-
-	  WebDriver driver=new ChromeDriver();
-}
 }
